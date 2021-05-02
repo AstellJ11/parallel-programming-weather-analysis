c28f35baa0a188348dc21d4b58d2e2d7c50de8d4
AstellJ11 <mrjamerjamer@gmail.com>
2021-05-02 23:43:48 +0100

    Minimum value found in parallel
    Implemented parallel 'min_reduce' kernel to find min value of input vector.

--- Solution1/kernels/my_kernels.cl
+++ Solution1/kernels/my_kernels.cl
@@ -1,5 +1,62 @@
-// COMMENT
-kernel void max_temp_kernel(global const int* A, global const int* B, global int* C) {
-	int id = get_global_id(0);
-	C[id] = *max_element(temp.begin(), temp.end());
+
+// Kernel to find the min value
+kernel void min_reduce(global const int* A, global int* B, local int *scratch) {
+  int id = get_global_id(0);
+  int lid = get_local_id(0);
+  int N = get_local_size(0);
+
+  // Cache all values from global to local memory
+  scratch[lid] = A[id];
+
+  // Wait for all local threads to finish copying from global to local memory
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  for (int i = 1; i < N; i *= 2)
+  {
+    if ((lid % (i * 2) == 0) && ((lid + i) < N))
+    {
+      if (scratch[lid + i] < scratch[lid])
+      {
+        scratch[lid] = scratch[lid + i];
+      }
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+  }
+
+  // Set result for the statistic
+  if (!lid)
+  {
+    atomic_min(B, scratch[lid]);
+  }
+}
+
+// Kernel to find the max value
+kernel void max_reduce(global const int* A, global int* B, local int *scratch) {
+  int id = get_global_id(0);
+  int lid = get_local_id(0);
+  int N = get_local_size(0);
+
+  // Cache all values from global to local memory
+  scratch[lid] = A[id];
+
+  // Wait for all local threads to finish copying from global to local memory
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  for (int i = 1; i > N; i *= 2)
+  {
+    if ((lid % (i * 2) == 0) && ((lid + i) > N))
+    {
+      if (scratch[lid + i] > scratch[lid])
+      {
+        scratch[lid] = scratch[lid + i];
+      }
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+  }
+
+  // Set result for the statistic
+  if (!lid)
+  {
+    atomic_max(B, scratch[lid]);
+  }
 }
--- Solution1/main.cpp
+++ Solution1/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char** argv) {
 	int platform_id = 0;
 	int device_id = 0;
 
+	// ############################# MAIN INIT #############################
+
 	try {
 		// Host operations - Select computing devices (GPU default)
 		cl::Context context = GetContext(platform_id, device_id);
@@ -39,20 +41,32 @@ int main(int argc, char** argv) {
 		catch (const cl::Error& err) {
 			cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << endl;
 			cout << "Build Options:\t" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << endl;
-			cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << :endl;
+			cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << endl;
 			throw err;
 		}
 
+		// Display device properties
+		cl::Device device = context.getInfo<CL_CONTEXT_DEVICES>()[0]; // Get device
+		cout << " " << endl;
+		cout << "Global Memory Size: " << device.getInfo <CL_DEVICE_GLOBAL_MEM_SIZE>() << endl;
+		cout << "Local Memory Size: " << device.getInfo <CL_DEVICE_LOCAL_MEM_SIZE>() << endl;
+		cout << " " << endl;
+		cout << "Max Work Group Size: " << device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << endl;
+		cout << "Max Device Dimensions: " << device.getInfo<CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS>() << endl;
+		cout << "Max Work Items Per Workgroup: " << device.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>() << endl;
+		cout << " " << endl;
 
-		// ############################# SERIAL CODE #############################
+		typedef int mytype;
 
 		// Declare all vector stores
-		vector<string> place = {};
-		vector<int> year = {};
-		vector<int> month = {};
-		vector<int> day = {};
-		vector<int> time = {};
-		vector<int> temp = {};
+		//vector<string> place = {};
+		//vector<int> year = {};
+		//vector<int> month = {};
+		//vector<int> day = {};
+		//vector<int> time = {};
+		vector<mytype> temperature = {};
+
+		// ############################# FILE IMPORTING #############################
 
 		// Input stream class for import file
 		ifstream myfile("temp_lincolnshire_short.txt");
@@ -71,88 +85,110 @@ int main(int argc, char** argv) {
 			}
 
 			int line_int = stoi(line_vec.back()); // Convert string to int
-			temp.push_back(line_int); // Add final string of each line to back of vector
+			temperature.push_back(line_int); // Add final string of each line to back of vector
 		}
 
+
+		// ############################# SERIAL CODE #############################
+
 		// Find the max element 
-		int max_temp = *max_element(temp.begin(), temp.end());
+		int max_temp = *max_element(temperature.begin(), temperature.end());
 
 		// Find the min element 
-		int min_temp = *min_element(temp.begin(), temp.end());
+		int min_temp = *min_element(temperature.begin(), temperature.end());
 
 		// Find the mean value
-		auto n = temp.size();
+		auto n = temperature.size();
 		float average = 0.0f;
 		if (n != 0) {
-			average = accumulate(temp.begin(), temp.end(), 0.0) / n;
+			average = accumulate(temperature.begin(), temperature.end(), 0.0) / n;
 		}
 
 		// Find the standard deviation value
 		float var = 0;
-		float numPoints = temp.size();
+		float numPoints = temperature.size();
 		for (n = 0; n < numPoints; n++)
 		{
-			var += (temp[n] - average) * (temp[n] - average);
+			var += (temperature[n] - average) * (temperature[n] - average);
 		}
 
 		var /= numPoints;
 		float sd = sqrt(var);
 
-		cout << "Max = " << max_temp << " Min = " << min_temp << " Mean = " << average << " SD = " << sd << endl;
+		cout << "Serial Outputs:";
+		cout << "\nMax = " << max_temp << " Min = " << min_temp << " Mean = " << average << " SD = " << sd << endl;
+		cout << " " << endl;
 
 		// ############################# SERIAL CODE ENDS #############################
 
-
 		// Memory allocation
+		// Host - input
+		size_t local_size = 32;
+		size_t padding_size = temperature.size() % local_size;
 
-		size_t vector_elements = temp.size();//number of elements
-		size_t vector_size = temp.size() * sizeof(int);//size in bytes
+		// Data padding
+		if (padding_size) {
+			// Create an extra vector with neutral values
+			vector<int> A_ext(local_size - padding_size, 0);
+			// Append that extra vector to our input
+			temperature.insert(temperature.end(), A_ext.begin(), A_ext.end());
+		}
 
-		// Result output
-		vector<int> C(vector_elements);
+		size_t input_elements = temperature.size();  // Number of input elements
+		size_t input_size = temperature.size() * sizeof(mytype);  // Size in bytes
+		size_t nr_groups = input_elements / local_size;
+
+		// Host - output
+		vector<mytype> B(input_elements);
+		size_t output_size = B.size() * sizeof(mytype);//size in bytes
 
 		// Device - buffers
-		cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, vector_size);
-		//cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, vector_size);
-		cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, vector_size);
+		cl::Buffer buffer_A(context, CL_MEM_READ_ONLY, input_size);
+		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, output_size);
+
+		// Device operations
+		// Copy array A to and initialise other arrays on device memory
+		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, input_size, &temperature[0]);
+
+		queue.enqueueFillBuffer(buffer_B, 0, 0, output_size); // Zero B buffer on device memory
+
+		// Setup and execute all kernels (i.e. device code)
+		// MIN KERNEL
+		cl::Kernel min_reduce = cl::Kernel(program, "min_reduce");
+		min_reduce.setArg(0, buffer_A);
+		min_reduce.setArg(1, buffer_B);
+		min_reduce.setArg(2, cl::Local(local_size * sizeof(mytype)));
+
+		// MAX KERNEL
+		//cl::Kernel maxReduce = cl::Kernel(program, "max_reduce");
+		//max_reduce.setArg(0, buffer_A);
+		//max_reduce.setArg(1, buffer_B);
+		//max_reduce.setArg(2, cl::Local(local_size * sizeof(mytype)));
 
 
-		// Device operations - Copy arrays A and B to device memory
-		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, vector_size, &temp[0], NULL, &A_event);
-		//queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, vector_size, &B[0], NULL, &B_event);
-
-
-		// Setup and execute the kernel
-		cl::Kernel kernel_max = cl::Kernel(program, "min_kernel");
-		kernel_max.setArg(0, buffer_A);
-		//kernel_add.setArg(1, buffer_B);
-		kernel_max.setArg(1, buffer_C);
-
-		// ADDITION: Execute the multiplication kernel
-		//cl::Kernel kernel_multadd = cl::Kernel(program, "multadd");
-		//kernel_multadd.setArg(0, buffer_A);
-		//kernel_multadd.setArg(1, buffer_B);
-		//kernel_multadd.setArg(2, buffer_C);
-
-		// COMMENT
-		queue.enqueueNDRangeKernel(kernel_max, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange, NULL, &prof_event);
+		// Call all kernels in a sequence
+		queue.enqueueNDRangeKernel(min_reduce, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
+		//queue.enqueueNDRangeKernel(max_reduce, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
 
 
 		// Copy the result from device to host
-		queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, vector_size, &C[0], NULL, &B_event);
+		queue.enqueueReadBuffer(buffer_B, CL_TRUE, 0, output_size, &B[0]);
 
-		//std::cout << "A = " << A << std::endl;
-		//std::cout << "B = " << B << std::endl;
-		//std::cout << "C = " << C << std::endl;
+		//cout << "A = " << temperature << endl;
+		//cout << "B = " << B << endl;
+
+		cout << "Parallel Outputs: ";
+		cout << "\nMin = " << B[0] << endl;
+		//cout << "Max = " << B[1] << endl;
 
 
 		// Display the kernel + upload/download execution time
-		cout << "\nKernel execution time for seperate kernels [ns]: " << prof_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << endl;
-		cout << "\nUpload time for vector A [ns]: " << A_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - A_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << endl;
-		cout << "Upload time for vector B [ns]: " << B_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - B_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << endl;
+		//cout << "\nKernel execution time for seperate kernels [ns]: " << prof_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - prof_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << endl;
+		//cout << "\nUpload time for vector A [ns]: " << A_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - A_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << endl;
+		//cout << "Upload time for vector B [ns]: " << B_event.getProfilingInfo<CL_PROFILING_COMMAND_END>() - B_event.getProfilingInfo<CL_PROFILING_COMMAND_START>() << endl;
 
-		// Display profiling information
-		cout << "\nProfiling information: " << GetFullProfilingInfo(prof_event, ProfilingResolution::PROF_US) << endl;
+		//// Display profiling information
+		//cout << "\nProfiling information: " << GetFullProfilingInfo(prof_event, ProfilingResolution::PROF_US) << endl;
 
 	}
 	catch (cl::Error err) {
